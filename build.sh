9e0cfbe2036d1592a078057c5011cf46386ddac8
Diwan <diwan6777@gmail.com>
2026-02-08 16:54:22 +0545

    Auto-initialize database and create superuser on deployment

--- build.sh
+++ build.sh
@@ -4,3 +4,13 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate --noinput
+
+# Create superuser automatically if it doesn't exist
+python manage.py shell << END
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'batukdiwan7@gmail.com', 'admin123')
+    print('Superuser created: username=admin, password=admin123')
+else:
+    print('Superuser already exists')
+END
